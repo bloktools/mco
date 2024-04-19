23882d7f955acf951389355a2bad3c63c97ba145
skibidiminecraft <167492278+skibidiminecraft@users.noreply.github.com>
2024-04-19 18:30:03 -0400

    super duper skibidi
    im catching it upt o main lol

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let globalSound = 1;
 let version = 'eagx';
 let shownversion = '1.8.8';
+let loginstatus = 0;
 const animatemeuwu = [
     '.bottom',
     '.bottom-background',
@@ -12,9 +13,11 @@ const animatemeuwu = [
     '.mccl',
     '.promo'
 ];
+$('#playbutton').hide();
 
 $('.versel').hide()
 $('.moreoptions').hide()
+picksplash();
 
 $('button').on('click', () => {
     if (globalSound==1 ) {
@@ -36,9 +39,15 @@ setInterval(function () {
         $('#audio').css('color', '#00ff00');
     } else {
         $('#audio').css('color', '#ff0000');
-
     }
+    loginstatus = localStorage.getItem('skibidiloginstatus');
 }, 200);
+
+setTimeout(function () {
+    $('#playbutton').show();
+
+    loginHandler()
+}, 300)
 
 function setver(ver, sver) {
     version = ver;
@@ -71,3 +80,78 @@ function playthegame() {
     console.log('eagerz/'+version+'.html')
     window.location.assign('eagerz/'+version+'.html') 
 }
+
+
+function picksplash() {
+    let r = Math.floor(Math.random()*splashes.length);
+    $('.splash').text(splashes[r]);
+}
+
+
+function loginHandler() {
+    if(!localStorage.getItem('skibidiloginstatus')){
+        localStorage.setItem('skibidiloginstatus', 0);
+        location.reload();
+    }
+    let sls = localStorage.getItem('skibidiloginstatus');
+    if (sls== 0 ){
+        $('#playbutton').hide();
+        $('.userinfo').text('you need to log in!')
+        $('.userinfo').append('<br><br><button onclick="login(0)">Log IN/sign up</button>');
+    }
+}
+
+$('.logincontainer').hide();
+
+function login(a) {
+    if (a==0) {
+$('.logincontainer').show();
+
+    } else if (a==-1) {
+$('.logincontainer').hide();
+
+    } else if (a==1) {
+        if ( $('#username').val() == localStorage.getItem('skibidiusername') ) {
+            if ( $('#password').val() == localStorage.getItem('skibidipassword') ) {
+                loginstatus=1;
+                $('.logincontainer').hide();
+                $('#playbutton').show();
+
+                $('.userinfo').html(` <p>Welcome, ${localStorage.getItem('skibidiusername')}!</p>
+                    <br><br><br>
+                    <button onclick="login(0)">Login screen</button>`);
+                    $('#delacc').show();
+                    $('.logincontainer>input').hide();
+            } else {
+                alert('wrong info!');
+                location.reload();
+            }
+        } else {
+            alert('wrong info!');
+            location.reload();
+        }
+    } else if (a==2) {
+        if (localStorage.getItem('skibidiusername')) {
+            alert('must delete old account first');
+            location.reload();
+        } else {
+            if (localStorage.getItem('skibidipassword')) {
+                alert('must delete old account first');
+                location.reload();
+            } else {
+
+                localStorage.setItem('skibidiusername', $('#username-su').val());
+                localStorage.setItem('skibidipassword', $('#password-su').val());
+                alert('account created .');
+                location.reload();
+            }
+
+        }
+
+    }
+}
+
+window.addEventListener("load", (e) => {
+    $('.loading').text('waitinf for scripts to finish');
+    setTimeout(()=>{$('.loading').hide()},300);
+  });  
